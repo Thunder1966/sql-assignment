4e34615b1e4a504798d237812da7eacd2ddd75cb
Thunder1966 <dhanushjagdish123@gmail.com>
2025-04-19 20:16:01 +0530

    SQL Assignment

---
+++ sql demo.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE Dhanush_ko19;
+CREATE DATABASE Rohan_ko19;
+CREATE DATABASE Skanda_ko19;
+CREATE DATABASE Sagar_ko19;
+
+use Dhanush_ko19;
+CREATE table Dhaush_details(id int,
+name varchar(30),
+usn int,
+gender varchar(2),
+height varchar(5),
+weight varchar(5),
+attendance bigint,
+Subject varchar(20),
+Marks varchar(20),
+Hobbies varchar(10),
+Sports varchar(10));
+
+use Rohan_ko19;
+CREATE table Rohan_details(id int,
+name varchar(30),
+usn int,
+gender varchar(2),
+height varchar(5),
+weight varchar(5),
+attendance bigint,
+Subject varchar(20),
+Marks varchar(20),
+Hobbies varchar(10),
+Sports varchar(10));
+
+use Skanda_ko19;
+CREATE table Skanda_details(id int,
+name varchar(30),
+usn int,
+gender varchar(2),
+height varchar(5),
+weight varchar(5),
+attendance bigint,
+Subject varchar(20),
+Marks varchar(20),
+Hobbies varchar(10),
+Sports varchar(10));
+
+use Sagar_ko19;
+CREATE table Sagar_details(id int,
+name varchar(30),
+usn int,
+gender varchar(2),
+height varchar(5),
+weight varchar(5),
+attendance bigint,
+Subject varchar(20),
+Marks varchar(20),
+Hobbies varchar(10),
+Sports varchar(10));
+CREATE table danny(id int,usn int);
+DELETE Table danny
